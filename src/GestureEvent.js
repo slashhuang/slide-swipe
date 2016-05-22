c42917f10dd64bac14ae5612a3ddddee610221f5
slashhuang <627284482@qq.com>
2016-05-22 16:39:20 +0800

    next move is to customize SwipeEvent

--- src/GestureEvent.js
+++ src/GestureEvent.js
@@ -43,6 +43,72 @@ export default class GestureEvent extends EventProto{
         this.ele.removeEventListener(type,func);
     }
     /**
+     * 设置滑动最后的信息
+     * @param e
+     */
+    setSliderLastInfo(e){
+        this.lastX = (utils.isMobile() ? e.touches[0].pageX : e.clientX);
+        this.lastY = (utils.isMobile() ? e.touches[0].pageY : e.clientY);
+    }
+    /**
+     * 设置滑动初始化的信息
+     * @param e
+     */
+    setStartInfo(e){
+        this.startX = (utils.isMobile() ? e.touches[0].pageX : e.clientX);
+        this.startY = (utils.isMobile() ? e.touches[0].pageY : e.clientY);
+        this.startTime = Date.now();
+    }
+    getSliderInfo(){
+        var absX = Math.abs(this.lastX - this.startX);
+        var absY = Math.abs(this.lastY - this.startY);
+        var moveTime = Date.now() - this.startTime;
+        var direction = absX > absY ? "x" : "y";
+        var positionInfo = {
+            x: this.lastX - this.startX >= 0 ? 'Right' : 'Left',
+            y: this.lastY - this.startY >= 0 ? 'Down' : 'Up'
+        };
+        return {
+            moveTime: moveTime,
+            positionInfo: positionInfo,
+            direction: direction,
+            moveX: absX,
+            moveY: absY,
+            startX: this.startX,
+            startY: this.startY,
+            lastX: this.lastX,
+            lastY: this.lastY
+        }
+    }
+    _touchStart(e){
+        //这里只允许单指操作，不会影响鼠标操作
+        if (e.touches && e.touches.length > 1) {
+            return false;
+        }
+        this.setStartInfo(e);
+        this.setSliderLastInfo(e);
+        //阻止默认滚动行为
+        e.preventDefault();
+    }
+    _touchMove(e){
+        this.setSliderLastInfo(e);
+        let getSliderInfo = this.getSliderInfo();
+        //触发swipeMove
+        this.trigger('swipeMove',[getSliderInfo,e.target]);
+        //阻止默认滚动行为
+        e.preventDefault();
+    }
+    _touchEnd(e){
+        let getSliderInfo = this.getSliderInfo();
+        let eventTypeName =  getSliderInfo[getSliderInfo['direction']];
+        //触发swipeLeft,swipeRight,swipeUp,swipeDown
+        this.trigger('swipe'+eventTypeName,[getSliderInfo,e.target]);
+        //阻止默认滚动行为
+        e.preventDefault();
+        //回滚实例数据
+        this.startX = this.startY = this.lastX = this.lastY = 0;
+    }
+    /**
      * 挂载事件
      */
     bindEvents(){
@@ -80,67 +146,5 @@ export default class GestureEvent extends EventProto{
         for(let key in finalEvents){
             this.bind(key,finalEvents[key].callback);
         }
-    }
-    /**
-     * 设置滑动最后的信息
-     * @param e
-     */
-    setSliderLastInfo(e){
-        this.lastX = (utils.isMobile() ? e.touches[0].pageX : e.clientX);
-        this.lastY = (utils.isMobile() ? e.touches[0].pageY : e.clientY);
-    }
-    /**
-     * 设置滑动初始化的信息
-     * @param e
-     */
-    setStartInfo(e){
-        this.startX = (utils.isMobile() ? e.touches[0].pageX : e.clientX);
-        this.startY = (utils.isMobile() ? e.touches[0].pageY : e.clientY);
-        this.startTime = Date.now();
-    }
-    _touchStart(e){
-        //这里只允许单指操作，不会影响鼠标操作
-        if (e.touches && e.touches.length > 1) {
-            return false;
-        }
-        this.setStartInfo(e);
-        this.setSliderLastInfo(e);
-        //阻止默认滚动行为
-        e.preventDefault();
-    }
-    getSliderInfo(){
-        var absX = Math.abs(this.lastX - this.startX);
-        var absY = Math.abs(this.lastY - this.startY);
-        var moveTime = Date.now() - this.startTime;
-        var direction = absX > absY ? "X" : "Y";
-        var positionInfo = {
-            x: this.lastX - this.startX >= 0 ? 'right' : 'left',
-            y: this.lastY - this.startY >= 0 ? 'down' : 'up'
-        };
-        return {
-            moveTime: moveTime,
-            positionInfo: positionInfo,
-            direction: direction,
-            moveX: absX,
-            moveY: absY,
-            startX: this.startX,
-            startY: this.startY,
-            lastX: this.lastX,
-            lastY: this.lastY
-        }
-    }
-    _touchMove(e){
-        this.setSliderLastInfo(e);
-        let getSliderInfo = this.getSliderInfo();
-        //触发swipeMove
-        this.trigger('swipeMove',[getSliderInfo,e.target]);
-        //阻止默认滚动行为
-        e.preventDefault();
-    }
-    _touchEnd(e){
-
-        //阻止默认滚动行为
-        e.preventDefault();
-        this.startX = this.startY = this.lastX = this.lastY = 0;
     }
 }
